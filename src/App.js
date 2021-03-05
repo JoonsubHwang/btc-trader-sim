9ae6767857447a4b16f63b0ee7d53c0a168578f9
JoonsubHwang <j97194719@gmail.com>
2021-03-05 09:45:03 +0900

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Homepage from './Homepage';
+import Trading from './Trading';
 
 function App() {
+
+  // return
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Switch>
+          <Route exact path='/' render={() => (
+            <Homepage></Homepage>
+          )} />
+          <Route exact path='/trading' render={() => (
+            <Trading></Trading>
+          )} />
+          <Route render={() => (
+              <p>not found</p> // <NotFound />
+          )}/>
+        </Switch>
       </header>
     </div>
   );
