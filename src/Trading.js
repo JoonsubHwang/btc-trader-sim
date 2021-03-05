1045eb3712bea130beb0aa714a75afbb8b763508
JoonsubHwang <j97194719@gmail.com>
2021-03-05 09:59:43 +0900

    Renamed UpdatePrice to Update

--- src/Trading.js
+++ src/Trading.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 
 function Trading(props) {
 
-    const iUpdatePrice = 100; // interval for updating price
+    const iUpdate = 100; // interval for updating 
     const [price, setPrice] = useState(0);
     const [balance, setBalance] = useState(props.balance);
     const [ownedBTC, setOwnedBTC] = useState(0);
@@ -10,12 +10,13 @@ function Trading(props) {
 
     // effect
     useEffect(() => {
-        const tUpdatePrice = setInterval(() => {
+
+        const tUpdate = setInterval(() => {
             setPrice(price + 1.0);
-        }, iUpdatePrice);
+        }, iUpdate);
 
         return ()=> { 
-            clearInterval(tUpdatePrice);
+            clearInterval(tUpdate);
         }
     });
 
