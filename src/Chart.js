368229a37a2e5b2b2727693b012ef8c9494a968a
JoonsubHwang <j97194719@gmail.com>
2021-03-07 10:15:26 +0900

    Added barUnitWidth, priceUnitHeight

--- src/Chart.js
+++ src/Chart.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 
 function Chart(props) {
 
+    const barUnitWidth = 10;
+    const priceUnitHeight = 50;
     const [timeUnit, setTimeUnit] = useState(15);
     const [priceUnit, setPriceUnit] = useState(50);
     const [barUnit, setBarUnit] = useState(1);
