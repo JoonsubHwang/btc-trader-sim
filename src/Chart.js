5579a9dcb8270bc339341a71b384484f5f1aea96
JoonsubHwang <j97194719@gmail.com>
2021-03-07 09:50:21 +0900

    Added barUnit, setBarUnit

--- src/Chart.js
+++ src/Chart.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 
 function Chart(props) {
 
+    const [barUnit, setBarUnit] = useState(1);
+
     // return
     return (
         <div className="chart">
