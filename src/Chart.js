4c1c298ce5708a0f31fc0b8513c72a6b5f2cf124
JoonsubHwang <j97194719@gmail.com>
2021-03-07 09:57:26 +0900

    Added timeUnit, setTimeUnit and priceUnit, setPriceUnit

--- src/Chart.js
+++ src/Chart.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 
 function Chart(props) {
 
+    const [timeUnit, setTimeUnit] = useState(15);
+    const [priceUnit, setPriceUnit] = useState(50);
     const [barUnit, setBarUnit] = useState(1);
 
     // return
