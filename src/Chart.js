c12e05e4da8afd0a58f8eaebad4b1fc60b4959a5
JoonsubHwang <j97194719@gmail.com>
2021-03-07 09:38:23 +0900

    Created Chart

---
+++ src/Chart.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+
+function Chart(props) {
+
+    // return
+    return (
+        <div className="chart">
+
+        </div>
+    );
+}
+
+export default Chart;
