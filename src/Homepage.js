adc4dcb842d94c24c60eeaadb1f444305cd0a4fb
JoonsubHwang <j97194719@gmail.com>
2021-03-07 10:24:56 +0900

    Changed className='homepage' to id='homepage-main'

--- src/Homepage.js
+++ src/Homepage.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Homepage() {
     return (
-        <div className='homepage'>
+        <div id='homepage-main'>
             <h1>BTC Trader Sim</h1>
             <Link to='/trading'> Trading </Link>
         </div>
