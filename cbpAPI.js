bf32cd8b393361cea794e1b68af5083aa3784fe2
JoonsubHwang <j97194719@gmail.com>
2021-05-08 15:18:30 -0400

    created CbProAPI and loadHistory()

--- cbpAPI.js
+++ cbpAPI.js
@@ -1,3 +1,24 @@
 // functions to communicate with Coinbase Pro API
 
+const hour = 3600;
+const min = 60;
 
+export class CbProAPI {
+
+    endpoint = 'https://api.pro.coinbase.com';
+    productID = 'BTC-USD';
+
+    // returns 6-hour historic rates of BTC-USD
+    loadHistory = async (currentTime) => {
+    
+        const path = this.endpoint + `/products/${this.productID}/candles?`;
+    
+        let res = await fetch(path + URLSearchParams({
+            start: currentTime - (6*hour),
+            end: currentTime,
+            granularity: min
+        }));
+
+        return await res.json();
+    }
+}
