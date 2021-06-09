8dec681b1cb79f52cbd821de67b67ae21f7348c4
JoonsubHwang <j97194719@gmail.com>
2021-06-09 14:30:23 -0400

    set path for .env file

--- src/BtcTraderSimDB.js
+++ src/BtcTraderSimDB.js
@@ -1,3 +1,3 @@
-require('dotenv').config()
+require('dotenv').config({ path: '../.env' })
 
 const connectionStr = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@btctradersimdb.xwwve.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
