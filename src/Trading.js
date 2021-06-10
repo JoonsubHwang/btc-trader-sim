9dd2c4f619467296953c65f16be069902324b00a
JoonsubHwang <j97194719@gmail.com>
2021-06-10 11:53:20 -0400

    added id #BTCWallet-grid on the div

--- src/Trading.js
+++ src/Trading.js
@@ -111,9 +111,9 @@ class Trading extends Component {
                             <p className='name'>Cash</p>
                             <p className='value'>{this.state.cash} USD</p>
                             <p className='name'>BTC Wallet</p>
-                            <div>
+                            <div id='BTCWallet-grid'>
                                 <p className='value'>{this.state.BTCWallet} BTC</p>
-                                <p className='value'>(≈ ${(this.state.BTCWallet * this.state.price).toFixed(0)})</p>
+                                <p className='value'>≈ {(this.state.BTCWallet * this.state.price).toFixed(0)} USD</p>
                             </div>
                         </div>
 
