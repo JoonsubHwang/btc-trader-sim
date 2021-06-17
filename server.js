a7513ad796a3887ac96dfdce1b950bf9dbe90cdd
JoonsubHwang <j97194719@gmail.com>
2021-06-17 16:27:14 -0400

    added json parser

--- server.js
+++ server.js
@@ -5,11 +5,22 @@ const app = express();
 
 const HTTP_PORT = process.env.PORT || 8080;
 
+
+
+// middlewares
+
 app.use(express.static('public'));
+
+app.use(express.json()); // json parser
 
 app.use((req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
+
+
+
+
+// on server start
 
 app.listen(HTTP_PORT, () => {
     console.log('listening on port: ' + HTTP_PORT);
