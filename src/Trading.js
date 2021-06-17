cdc13ef5c9b06f5cc08cee11596ea32fb0bf8e81
JoonsubHwang <j97194719@gmail.com>
2021-06-17 15:32:06 -0400

    substituted orderAmount input's defaultValue with value

--- src/Trading.js
+++ src/Trading.js
@@ -150,7 +150,7 @@ class Trading extends Component {
                             <div id='orderAmount-grid'>
                                 <input className='value' type='number' name='orderAmount' 
                                     step={(this.state.cash * 0.1 / this.state.orderPrice).toFixed(4)} 
-                                    defaultValue='0' min='0' max={this.state.cash / this.state.orderPrice} onChange={this.setOrderAmount}></input>
+                                    value={this.state.orderAmountBTC} min='0' max={this.state.cash / this.state.orderPrice} onChange={this.setOrderAmount}></input>
                                 <p className='name'> BTC</p>
                                 <p className='value'>≈ {((this.state.orderType === this.orderTypes.MARKET_ORDER ? this.state.price : this.state.orderPrice) 
                                                             * this.state.orderAmountBTC).toFixed(2)}</p>
