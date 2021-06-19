65f40e18f20ab8aa0eac18d041a0a0cad349d7ec
JoonsubHwang <j97194719@gmail.com>
2021-06-19 14:40:03 -0400

    shorted the code and edited error messages

--- src/CbProAPI.js
+++ src/CbProAPI.js
@@ -69,31 +69,29 @@ export class CbProAPI {
 
             let res = await fetch(path);
 
-            // format object properties
             let data = await res.json();
 
-            if (!data || data.length === 0)
-                throw new Error(`Failed to fetch a candle from ${path}`);
+            if (!data || data[0].length != 6)
+                throw new Error(`Failed to fetch a candle from ${path}. (data[0].length: ` +data[0].length + `)`);
             
-            data = data.map(rate => {
-                return {
-                    // Unix Timestamp to local date 
-                    time: new Date(new Date(rate[0]*1000).toLocaleString(locale)),
-                    low: rate[1],
-                    high: rate[2],
-                    open: rate[3],
-                    close: rate[4],
-                    openVolume: rate[4] > rate[3] ? 0 : rate[5],
-                    valueVolume: rate[4] > rate[3] ? rate[5] : 0,
-                    highVolume: rate[5]
-                };
-            })
+            // format object properties
+            data = {
+                // Unix Timestamp to local date 
+                time: new Date(new Date(data[0][0]*1000).toLocaleString(locale)),
+                low: data[0][1],
+                high: data[0][2],
+                open: data[0][3],
+                close: data[0][4],
+                openVolume: data[0][4] > data[0][3] ? 0 : data[0][5],
+                valueVolume: data[0][4] > data[0][3] ? data[0][5] : 0,
+                highVolume: data[0][5]
+            };
 
-            return data[0];
+            return data;
         }
         catch(err) {
-            console.error(err);
-            throw err;
+            console.error('[CbProAPI] ' + err);
+            throw 'Error: Failed to load a candle from CbProAPI.';
         }
     }
 
