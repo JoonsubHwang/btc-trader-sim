29f9e2407cad7193f5b123d85c62cc7170e4847e
JoonsubHwang <j97194719@gmail.com>
2021-06-23 22:24:18 -0400

    changed --bg color

--- src/Chart.js
+++ src/Chart.js
@@ -7,7 +7,7 @@ import { CbProAPI } from './CbProAPI';
 // colors
 const white = 'rgb(250, 250, 250)';
 const grey = 'rgb(120, 120, 120)';
-const bg = 'rgb(20, 25, 35)';
+const bg = 'rgb(12, 26, 41)';
 const green = 'rgb(20, 180, 140)';
 const red = 'rgb(250, 70, 90)';
 const bgYellow = 'rgb(255, 255, 0)';
