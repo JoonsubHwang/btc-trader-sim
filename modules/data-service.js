6e264d73a3158e4b1be064cb925264f184e07309
JoonsubHwang <j97194719@gmail.com>
2021-06-29 21:16:19 -0400

    added initialize()

--- modules/data-service.js
+++ modules/data-service.js
@@ -1,5 +1,23 @@
 const mongoose = require('mongoose');
 const accountSchema = require('./accountSchema.js');
 
+module.exports = (connStr) => {
 
+    let Accounts;
+
+    return {
+
+        initialize: async () => {
+            let db = mongoose.createConnection(connStr, { useNewUrlParser: true, useUnifiedTopology: true });
+            db.on('error', () => {
+                throw new Error('[data-service] Failed to connect to the database');
+            });
+            db.once('open', () => {
+                Accounts = db.model('Accounts', accountSchema);
+                return;
+            })
+        } 
+           
+    }
+}
 
