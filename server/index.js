f88dee89b93b0d10e994bc2ebb111b9ba5bfe9eb
JoonsubHwang <j97194719@gmail.com>
2021-07-01 17:53:09 -0400

    added /sign-in POST route and signin()

--- server/index.js
+++ server/index.js
@@ -1,21 +1,57 @@
 const express = require('express');
 const path = require('path');
+const dataService = require('../modules/data-service');
+
+
 
 const app = express();
-
 const HTTP_PORT = process.env.PORT || 8080;
 
 
 
 // middlewares
 
-app.use(express.static(path.join(__dirname + '/../build')));
-
+app.use(express.static(path.join(__dirname + '/../build'))); // public folder
 app.use(express.json()); // json parser
 
-app.use((req, res) => {
+
+
+
+// routes
+
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/../build/index.html'));
 });
+
+app.post('/sign-in', (req, res) => {
+
+    const signInData = req.body;
+    dataService.validateSignIn(signInData)
+        .then(invalid => {
+            if (invalid)
+                res.send(invalid);
+            else {
+                signIn(signInData.email);
+                res.redirect('back');
+            }
+        })
+        .catch(err => {
+            console.error('[server] Failed to sign in. ' + err);
+            res.send({ error: 'Server had a problem signing in.' });
+        });
+
+});
+
+
+
+
+
+// helpers
+
+function signIn(email) {
+    // TODO: save email to session
+    console.log('[debug]: Signed in with ' + email);
+}
 
 
 
