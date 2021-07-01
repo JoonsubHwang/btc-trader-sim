43780ce8e381a9575c20feb5d63c0600c29db122
JoonsubHwang <j97194719@gmail.com>
2021-07-01 18:16:14 -0400

    separated a function into multiple ones

--- modules/data-service.js
+++ modules/data-service.js
@@ -1,67 +1,63 @@
 const mongoose = require('mongoose');
 const accountSchema = require('./accountSchema.js');
 
-module.exports = (connStr) => {
+const connStr = '';
+let Accounts;
 
-    let Accounts;
 
-    return {
 
-        initialize: async () => {
-            let db = mongoose.createConnection(connStr, { useNewUrlParser: true, useUnifiedTopology: true });
-            db.on('error', () => {
-                throw new Error('[data-service] Failed to connect to the database');
-            });
-            db.once('open', () => {
-                Accounts = db.model('Accounts', accountSchema);
-                return;
-            })
-        },
+exports.connect = async () => {
+    let db = mongoose.createConnection(connStr, { useNewUrlParser: true, useUnifiedTopology: true });
+    db.on('error', () => {
+        throw new Error('[data-service] Failed to connect to the database');
+    });
+    db.once('open', () => {
+        Accounts = db.model('Accounts', accountSchema);
+        return;
+    })
+},
 
-        findAccount: async (email) => {
-            try {
-                // TODO: apply hashing
-                return await Accounts.findOne({ email: email }).lean().exec();
-            } catch (err) {
-                console.error('[data-service] Failed to find the account. ' + err);
-                throw new Error('Failed to find the account.');
-            }
-        },
-
-        validateSignIn: async (signInData) => {
-
-            let invalid = {};
-
-            try {
-
-                if (signInData.email) { // TODO: apply hashing
-
-                    const account = await findAccount(signInData.email);
-    
-                    if (account) {
-                        if (account.password === signInData.password) // TODO: apply hashing
-                            return undefined; // only successful sign in
-                        else
-                            invalid.password = 'Password is incorrect.';
-                    }
-                    else
-                        invalid.email = 'Account does not exist.';
-    
-                }
-                else 
-                    invalid.email = 'Please enter the email.';
-
-            } catch (err) {
-                console.error('[data-service]: Failed to validate sign in data. ' + err);
-                throw new Error('Failed to validate sign in data.');
-            }
-
-            if (!signInData.password)
-                invalid.password = 'Please enter the password';
-
-            return invalid;
-        }
-
+exports.findAccount = async (email) => {
+    try {
+        // TODO: apply hashing
+        return await Accounts.findOne({ email: email }).lean().exec();
+    } catch (err) {
+        console.error('[data-service] Failed to find the account. ' + err);
+        throw new Error('Failed to find the account.');
     }
+},
+
+exports.validateSignIn = async (signInData) => {
+
+    let invalid = {};
+
+    try {
+
+        if (signInData.email) { // TODO: apply hashing
+
+            const account = await Accounts.findOne({ email: email }).lean().exec();
+
+            if (account) {
+                if (account.password === signInData.password) // TODO: apply hashing
+                    return undefined; // only successful sign in
+                else
+                    invalid.password = 'Password is incorrect.';
+            }
+            else
+                invalid.email = 'Account does not exist.';
+
+        }
+        else 
+            invalid.email = 'Please enter the email.';
+
+    } catch (err) {
+        console.error('[data-service] Failed to validate sign in data. ' + err);
+        throw new Error('Failed to validate sign in data.');
+    }
+
+    if (!signInData.password)
+        invalid.password = 'Please enter the password';
+
+    return invalid;
 }
 
