a051d57c5e06f764a2911d884e3870eba12c3297
JoonsubHwang <j97194719@gmail.com>
2021-07-01 17:13:29 -0400

    added findAccount() and validateSignIn()

--- modules/data-service.js
+++ modules/data-service.js
@@ -16,8 +16,46 @@ module.exports = (connStr) => {
                 Accounts = db.model('Accounts', accountSchema);
                 return;
             })
-        } 
-           
+        },
+
+        findAccount: async (email) => {
+            try {
+                // TODO: apply hashing
+                return await Accounts.findOne({ email: email }).lean().exec();
+            } catch (err) {
+                console.error('[data-service] Failed to find the account. ' + err);
+                throw new Error('Failed to find the account.');
+            }
+        },
+
+        validateSignIn: async (signInData) => {
+
+            let incorrect = {};
+
+            if (signInData.email) {
+
+                const account = await findAccount(signInData.email);
+
+                if (account) {
+                    // TODO: apply hashing
+                    if (account.password === signInData.password)
+                        return; // only successful sign in
+                    else
+                        incorrect.password = 'Password is incorrect.';
+                }
+                else
+                    incorrect.email = 'Account does not exist.';
+
+            }
+            else 
+                incorrect.email = 'Please enter the email.';
+
+            if (!signInData.password)
+                incorrect.password = 'Please enter the password';
+
+            return incorrect;
+        }
+
     }
 }
 
