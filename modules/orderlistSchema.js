87b2ae8042b7ecca3d1d10e6284a0bc2d87545d2
JoonsubHwang <j97194719@gmail.com>
2021-07-06 17:01:20 -0400

    added buy to pending and history

--- modules/orderlistSchema.js
+++ modules/orderlistSchema.js
@@ -4,11 +4,13 @@ let Schema = mongoose.Schema;
 module.exports = new Schema({
     email: String,
     pendings: [{
+        buy: Boolean,
         type: String,
         price: Number,
         amount: Number
     }],
     history: [{
+        buy: Boolean,
         type: String,
         price: Number,
         amount: Number
