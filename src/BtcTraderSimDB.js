b8dbfd8bc0baddb9019b7504b173fe1d1a2aa95c
JoonsubHwang <j97194719@gmail.com>
2021-07-16 13:04:25 -0400

    deleted BtcTraderSimDB

--- src/BtcTraderSimDB.js
+++
@@ -1,3 +0,0 @@
-require('dotenv').config({ path: '../.env' })
-
-const connectionStr = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@btctradersimdb.xwwve.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
