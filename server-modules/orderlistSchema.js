ee3f03874a373284345c720dbee5c343ad309510
JoonsubHwang <j97194719@gmail.com>
2021-07-21 15:31:47 -0400

    removed pendings and renamed order record properties

--- server-modules/orderlistSchema.js
+++ server-modules/orderlistSchema.js
@@ -3,16 +3,11 @@ let Schema = mongoose.Schema;
 
 module.exports = new Schema({
     email: String,
-    pendings: [{
-        buy: Boolean,
-        type: String,
-        price: Number,
-        amount: Number
-    }],
     history: [{
         buy: Boolean,
-        type: String,
-        price: Number,
-        amount: Number
+        orderType: String,
+        orderPrice: Number,
+        orderAmount: Number,
+        orderTime: Date
     }]
 });
