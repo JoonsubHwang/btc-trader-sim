b9a24ea310c6222178c3f9ccccfa56e20489d53f
JoonsubHwang <j97194719@gmail.com>
2021-07-23 16:53:47 -0400

    Created Orders component

---
+++ src/Orders.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Close } from '@material-ui/icons';
+import './Orders.css';
+
+export default class Orders extends React.Component {
+
+    render = () =>
+        <div id='orders-main'>
+            <div id='orders-popup'>
+
+                <Close id='ordersClose-btn' onClick={this.props.toggleOrdersPopup}></Close>
+
+                <div id='orders-content'>
+
+                    <h2 id='orders-heading' className='large'>Order History</h2>
+
+                    <div id='orders-frame'>
+                        {(this.props.orderHistory) ?
+                            this.props.orderHistory.map(order => {
+
+                                const time = new Date(order.orderTime).toLocaleString('en-US');
+
+                                return  <div key={'order-' + order._id}>
+                                            {order.buy ?
+                                            <p className='green'>BUY</p>
+                                            : <p className='red'>SELL</p>}
+                                            <p className='value'>{order.orderType.substring(0,6)}</p>
+                                            <p className='value'>{order.orderPrice.toFixed(2)}</p>
+                                            <p className='value'>{order.orderAmount.toFixed(3)}</p>
+                                            <p className='value'>{time.substring(0,time.length-6) + time.substring(time.length-3, time.length)}</p>
+                                        </div>
+                            })
+                            : null
+                        }
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+}
