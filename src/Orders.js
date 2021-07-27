7d9edc7f53483b000b44ff7dadd33204abb0e445
JoonsubHwang <j97194719@gmail.com>
2021-07-27 16:34:15 -0400

    added #noOrder-msg

--- src/Orders.js
+++ src/Orders.js
@@ -16,21 +16,24 @@ export default class Orders extends React.Component {
 
                     <div id='orders-frame'>
                         {(this.props.orderHistory) ?
-                            this.props.orderHistory.map(order => {
+                            ( (this.props.orderHistory.length > 0) ?
+                                this.props.orderHistory.map(order => {
 
-                                const time = new Date(order.orderTime).toLocaleString('en-US');
+                                    const time = new Date(order.orderTime).toLocaleString('en-US');
 
-                                return  <div key={'order-' + order._id}>
-                                            {order.buy ?
-                                            <p className='green'>BUY</p>
-                                            : <p className='red'>SELL</p>}
-                                            <p className='value'>{order.orderType.substring(0,6)}</p>
-                                            <p className='value'>{order.orderPrice.toFixed(2)}</p>
-                                            <p className='value'>{order.orderAmount.toFixed(3)}</p>
-                                            <p className='value'>{time.substring(0,time.length-6) + time.substring(time.length-3, time.length)}</p>
-                                        </div>
-                            })
-                            : null
+                                    return  <div key={'order-' + order._id}>
+                                                {order.buy ?
+                                                <p className='green'>BUY</p>
+                                                : <p className='red'>SELL</p>}
+                                                <p className='value'>{order.orderType.substring(0,6)}</p>
+                                                <p className='value'>{order.orderPrice.toFixed(2)}</p>
+                                                <p className='value'>{order.orderAmount.toFixed(3)}</p>
+                                                <p className='value'>{time.substring(0,time.length-6) + time.substring(time.length-3, time.length)}</p>
+                                            </div>
+                                })
+                                : <p id='noOrder-msg'>No order has been made.</p>
+                            )
+                            : <p id='noOrder-msg'>Order history is undefined.</p>
                         }
                     </div>
 
