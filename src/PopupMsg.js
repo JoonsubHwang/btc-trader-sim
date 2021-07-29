7ae2c8f3a98c4d7f20382a50c779cc8deb11f9ad
JoonsubHwang <j97194719@gmail.com>
2021-07-29 13:02:28 -0400

    separated #PopupMsg-main and #PopupMsg-grid

--- src/PopupMsg.js
+++ src/PopupMsg.js
@@ -6,20 +6,22 @@ export default class PopupMsg extends React.Component {
     
     render = () =>
         <div id='PopupMsg-main'>
-            
-            {/* success/error icon */}
-            {this.props.popupMsg.success ?
-            <CheckCircle id='success-icon'/>
-            : <ErrorOutline id='error-icon'/>}
+            <div id='PopupMsg-grid'>
+                
+                {/* success/error icon */}
+                {this.props.popupMsg.success ?
+                <CheckCircle id='success-icon'/>
+                : <ErrorOutline id='error-icon'/>}
 
-            <div>
-                {/* Success/Error */}
-                <p>{this.props.popupMsg.success ?
-                <span id='success'>Success </span>
-                : <span id='error'>Error </span>}
-                {/* message */}
-                {this.props.popupMsg.message}</p>
+                <div>
+                    {/* Success/Error */}
+                    <p>{this.props.popupMsg.success ?
+                    <span id='success'>Success </span>
+                    : <span id='error'>Error </span>}
+                    {/* message */}
+                    {this.props.popupMsg.message}</p>
+                </div>
+
             </div>
-
         </div>
 }
